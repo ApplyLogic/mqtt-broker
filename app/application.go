31bfb097d07ddbf707fc21e7286f5eb47027d69f
Golan Shay <golanshy@gmail.com>
2025-03-17 16:43:29 +0000

    refactor code

---
+++ app/application.go
@@ -0,0 +1,48 @@
+package app
+
+import (
+	"github.com/ApplyLogic/mqtt-broker/config"
+	"github.com/ApplyLogic/mqtt-broker/internal/middleware"
+	"github.com/ApplyLogic/mqtt-broker/mqtt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+type App struct {
+	Config *config.Config
+	Broker *mqtt.Broker
+	Logger *middleware.Logger
+}
+
+func (a *App) Initialize(cfg *config.Config) {
+	// Initialize Logger
+	a.Config = cfg
+	a.Logger = &middleware.Logger{}
+	a.Logger.Initialize(cfg)
+	a.Broker = mqtt.NewBroker(a.Config)
+}
+
+func (a *App) Start() {
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-sigs
+		done <- true
+	}()
+
+	go func() {
+		err := a.Broker.Server.Serve()
+		if err != nil {
+			log.Fatal(err)
+		}
+		a.Broker.Server.Log.Info("Broker started")
+	}()
+
+	<-done
+	a.Broker.Server.Log.Warn("caught signal, stopping...")
+	_ = a.Broker.Server.Close()
+	a.Broker.Server.Log.Info("main.go finished")
+}
---
+++ mqtt/broker.go
@@ -0,0 +1,85 @@
+package mqtt
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"github.com/ApplyLogic/mqtt-broker/config"
+	mqttServer "github.com/mochi-mqtt/server/v2"
+	"github.com/mochi-mqtt/server/v2/hooks/auth"
+	"github.com/mochi-mqtt/server/v2/listeners"
+	"log"
+	"os"
+)
+
+type Broker struct {
+	Server *mqttServer.Server
+}
+
+func NewBroker(cfg *config.Config) *Broker {
+
+	// Load tls cert from your cert file
+	cert, err := tls.LoadX509KeyPair(fmt.Sprintf("%s/test_cert.pem", cfg.TLS.CertPath), fmt.Sprintf("%s/test_cert.key", cfg.TLS.CertPath))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Basic TLS Config
+	tlsConfig := &tls.Config{
+		Certificates: []tls.Certificate{cert},
+	}
+
+	// Optionally, if you want clients to authenticate only with certs issued by your CA,
+	// you might want to use something like this:
+	if cfg.TLS.CACertFile != "" {
+		pemCACert, err := os.ReadFile(cfg.TLS.CACertFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		certPool := x509.NewCertPool()
+		ok := certPool.AppendCertsFromPEM(pemCACert)
+		if ok {
+			tlsConfig.ClientAuth = tls.RequireAndVerifyClientCert
+			tlsConfig.ClientCAs = certPool
+		}
+	}
+
+	server := mqttServer.New(nil)
+	_ = server.AddHook(new(auth.AllowHook), nil)
+
+	tcp := listeners.NewTCP(listeners.Config{
+		ID:      cfg.MQTT.TCP.Id,
+		Address: fmt.Sprintf(":%s", cfg.MQTT.TCP.Address),
+		//TLSConfig: tlsConfig,
+	})
+	err = server.AddListener(tcp)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ws := listeners.NewWebsocket(listeners.Config{
+		ID:      cfg.MQTT.WebSocket.Id,
+		Address: fmt.Sprintf(":%s", cfg.MQTT.WebSocket.Address),
+		//TLSConfig: tlsConfig,
+	})
+	err = server.AddListener(ws)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	stats := listeners.NewHTTPStats(
+		listeners.Config{
+			ID:      cfg.MQTT.HTTPStats.Id,
+			Address: fmt.Sprintf(":%s", cfg.MQTT.HTTPStats.Address),
+			//TLSConfig: tlsConfig,
+		}, server.Info,
+	)
+	err = server.AddListener(stats)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &Broker{
+		Server: server,
+	}
+}
